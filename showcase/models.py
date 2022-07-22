4fa88d7943aa568ff6ec7b3b69159f68b30c1aeb
nafnaufal <naufalanbial@gmail.com>
2022-07-22 19:09:48 +0700

    make admin and database sqlite3

--- showcase/models.py
+++ showcase/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    npm = models.CharField(max_length=11)
+    name = models.CharField(max_length=64)
+    gen = models.IntegerField()
+
+class Project(models.Model):
+    title = models.CharField(max_length=64)
+    desc = models.CharField(max_length=1000)
+    link_yt = models.CharField(max_length=64)
+    link_cv = models.CharField(max_length=500)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='made_by')
