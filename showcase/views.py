19093712ebf75efe70e80edd7ded997082cb9c93
nafnaufal <naufalanbial@gmail.com>
2022-07-22 19:34:23 +0700

    clean database name

--- showcase/views.py
+++ showcase/views.py
@@ -3,14 +3,6 @@ from django.shortcuts import render
 from .models import *
 # Create your views here.
 
-card = [
-    ["Website Warga Bantu Warga", "Sally Rally"],
-    ["Aplikasi Deteksi Dini Gejala Covid-19", "Sally Rally"],
-    ["Aplikasi 1", "Sally Rally"],
-    ["Aplikasi 2", "Sally Rally"],
-    ["Aplikasi 3", "Sally Rally"]
-]
-
 def index(request):
     return render(request, 'showcase/index.html');
 
