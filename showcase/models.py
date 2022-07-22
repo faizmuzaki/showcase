eee784e1aeee483cab6e6db313f62acfabf94d15
nafnaufal <naufalanbial@gmail.com>
2022-07-22 19:30:02 +0700

    Revert "manage database"
    This reverts commit bacb7efcd0ca77283227602a41488ff1930f9fd2.

--- showcase/models.py
+++ showcase/models.py
@@ -3,18 +3,12 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     npm = models.CharField(max_length=11)
-    nama = models.CharField(max_length=64)
-    angkatan = models.IntegerField()
-
-    def __str__(self):
-       return self.nama
+    name = models.CharField(max_length=64)
+    gen = models.IntegerField()
 
 class Project(models.Model):
-    judul = models.CharField(max_length=64)
-    deskripsi = models.CharField(max_length=1000)
-    link_youtube = models.CharField(max_length=64)
-    link_cover = models.CharField(max_length=500)
+    title = models.CharField(max_length=64)
+    desc = models.CharField(max_length=1000)
+    link_yt = models.CharField(max_length=64)
+    link_cv = models.CharField(max_length=500)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='made_by')
-
-    def __str__(self):
-       return self.judul
