ee69161bc37634e6d53429e1df24ded1a469b3c2
kaylaatsila <kayla.atsilaivanka2008@students.unila.ac.id>
2022-08-02 21:06:04 +0700

    imud

--- showcase/views.py
+++ showcase/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from django.db.models import Q
-import requests
 from .models import *
 import random
 # Create your views here.
